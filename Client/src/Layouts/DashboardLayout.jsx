104066b8a0c9381769836a1c7bb5df1cf790d2d4
itsypalaak53 <palakkawarchouhan@gmail.com>
2026-08-04 18:28:33 +0530

    adding more

--- Client/src/Layouts/DashboardLayout.jsx
+++ Client/src/Layouts/DashboardLayout.jsx
@@ -7,3 +7,13 @@ const DashboardLayout = () => {
     <div className="flex min-h-screen bg-[#FFF9F5]">
       {/* Sidebar Section */}
       <div className="w-64 bg-[#FFF9F5] p-6 border-r border-slate-200/60 flex flex-col justify-between shadow-xs"></div>
+      <div>
+          {/* Logo Section (Text hata diya hai, sirf image hai) */}
+          <div className="flex flex-col items-center mb-6 px-2">
+            <img src={logo} alt="Clickmart Logo" className="w-28 h-28 object-contain" />
+          </div>
+
+          {/* Navigation Links */}
+          <div className="flex flex-col gap-2.5">
+            <Link 
+              to="/dashboard"
